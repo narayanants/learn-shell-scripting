b470baab7f916f9d6c42f6173756beb88498100b
Narayanan Ts <tsnarayanan7@gmail.com>
2019-08-08 17:43:57 +0530

    exercise1

---
+++ Fundamentals/exercise1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CUR_YEAR=`date +"%Y"`
+echo $CUR_YEAR
+echo -n "Enter your name:"
+read NAME
+echo -n "Enter your age"
+read AGE
+let FIF_YEAR=(50-$AGE)+$CUR_YEAR
+echo Hello $NAME !! You will be 50 years old in $FIF_YEAR
