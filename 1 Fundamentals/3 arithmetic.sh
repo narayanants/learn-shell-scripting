139b302746f39072de52e605cefd29992ca4437b
Narayanan Ts <tsnarayanan7@gmail.com>
2019-08-09 12:49:51 +0530

    arithmetic

---
+++ 1 Fundamentals/3 arithmetic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "Please enter some value: \c"
+read -r a
+echo -e "Please enter another value: \c"
+read -r b
+
+echo "a+b value is $(($a+$b))"
+echo "a-b value is $(($a-$b))"
+echo "axb value is $(($a*$b))"
+echo "a/b value is $(($a/$b))"
+echo "a%b value is $(($a%$b))"
+
+echo "Completed successfully"
